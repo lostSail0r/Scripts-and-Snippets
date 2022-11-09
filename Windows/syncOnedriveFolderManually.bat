4bafb3d69e48eaad40ce376d3eac65c4b9e6af27
Chris Grady <help@cgfixit.com>
2022-11-08 21:30:56 -0500

    Update syncOnedriveFolderManually.bat

--- Windows/syncOnedriveFolderManually.bat
+++ Windows/syncOnedriveFolderManually.bat
@@ -1,1 +1,3 @@
-
+REM ** Add any folder to onedrive sync **
+mklink /j "%UserProfile%\PathForYourOnePath\C-Tools" "C:\Tools"
+REM                                         ^that is the folder you want it to show up under onedrive sync folder from
