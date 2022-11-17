aed3b5fcc205f8ceb4a23695c0b4ca8eabc12e15
Chris Grady <help@cgfixit.com>
2022-11-17 11:46:03 -0500

    Update inputLastLogon.bat

--- Windows/AD/inputLastLogon.bat
+++ Windows/AD/inputLastLogon.bat
@@ -1,6 +1,7 @@
-REM - NOTE: You must be connected to domain network (or at least have DC as DNS server if theres a network path; sometimes connecting to corp VPN will also work)
-REM - Note: If you are running this against a local account, remove the "/domain" from line 10
 @echo off
+
+REM - NOTE: You must be connected to domain network (or at least have DC as DNS server if theres a network path; sometimes connecting to corp VPN will also work)
+REM - Note: If you are running this against a local account, remove the "/domain" from line 11
 
 echo Find User's Last Login Time:
 echo ============================
